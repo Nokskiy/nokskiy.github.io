3869d662dd76e41d325a8292f32d37f7445185f4
Nokskiy <145178078+Nokskiy@users.noreply.github.com>
2026-01-08 15:49:57 +0500

    Text writing

---
+++ scripts/textEffects.js
@@ -0,0 +1,28 @@
+class TextEffectsManager {
+    GetText(elementId) {
+        return document.getElementById(elementId).textContent;
+    }
+    
+    SetText(elementId, elementText) {
+        document.getElementById(elementId).textContent = elementText;
+    }
+
+    AddSymbols(elementId, symbol) {
+        this.SetText(elementId,this.GetText(elementId) + symbol)
+    }
+
+    async WriteText(elementId) {
+        var textContent = this.GetText(elementId);
+        this.SetText(elementId,"");
+        for(let i = 0; i < textContent.length;i++) {
+            this.AddSymbols(elementId, textContent[i]);
+            await new Promise(resolve => setTimeout(resolve,100));
+        }
+    }
+
+    constructor() {
+        this.WriteText("nick");
+    }
+}
+
+var textEffectsManager = new TextEffectsManager();
